c47d8b690e248bd6c19502c51a0477dfeb7abbe5
abhishek155 <abhishek.kashyap1443@gmail.com>
2018-07-10 00:02:33 +0530

    1st attempt

---
+++ rough.py
@@ -0,0 +1,80 @@
+from tkinter import*
+import random
+import time
+
+root = Tk()
+root.geometry("1600x700+0+0")
+root.title("Fast Food Hub")
+
+Tops = Frame(root,bg="black",width = 1600,height=50,relief=RAISED)
+Tops.pack(side=TOP)
+
+f1 = Frame(root,width = 900,height=700,relief=RAISED)
+f1.pack(side=LEFT)
+
+f2 = Frame(root ,width = 400,height=700,relief=RAISED)
+f2.pack(side=RIGHT)
+########## current day and current time #########
+localtime=time.asctime(time.localtime(time.time()))
+######### information of top ####################
+lblinfo = Label(Tops, font=( 'aria' ,30, 'bold' ),text="Fast Food Hub",fg="dark red",bd=10,anchor='w')
+lblinfo.grid(row=0,column=0)
+lblinfo = Label(Tops, font=( 'aria' ,20, ),text=localtime,fg="dark red",anchor=W)
+lblinfo.grid(row=1,column=0)
+############ calculator #################
+text_Input=StringVar()
+operator =""
+
+txtdisplay = Entry(f2,font=('ariel' ,20,'bold'), textvariable=text_Input , bd=5 ,insertwidth=7 ,bg="black",justify='right')
+txtdisplay.grid(columnspan=4)
+
+def  btnclick(numbers):
+    global operator
+    operator=operator + str(numbers)
+    text_Input.set(operator)
+
+def clrdisplay():
+    global operator
+    operator=""
+    text_Input.set("")
+
+def equals():
+    global operator
+    sumup=str(eval(operator))
+
+    text_Input.set(sumup)
+    operator = ""
+
+def Ref():
+    x=random.randint(12980, 50876)
+    randomRef = str(x)
+    rand.set(randomRef)
+
+    cof =float(Fries.get())
+    colfries= float(Largefries.get())
+    cob= float(Burger.get())
+    cofi= float(Filet.get())
+    cochee= float(Cheese_burger.get())
+    codr= float(Drinks.get())
+
+    costoffries = cof*25
+    costoflargefries = colfries*40
+    costofburger = cob*35
+    costoffilet = cofi*50
+    costofcheeseburger = cochee*50
+    costofdrinks = codr*35
+
+    costofmeal = "Rs.",str('%.2f'% (costoffries +  costoflargefries + costofburger + costoffilet + costofcheeseburger + costofdrinks))
+    PayTax=((costoffries +  costoflargefries + costofburger + costoffilet +  costofcheeseburger + costofdrinks)*0.33)
+    Totalcost=(costoffries +  costoflargefries + costofburger + costoffilet  + costofcheeseburger + costofdrinks)
+    Ser_Charge=((costoffries +  costoflargefries + costofburger + costoffilet + costofcheeseburger + costofdrinks)/99)
+    Service="Rs.",str('%.2f'% Ser_Charge)
+    OverAllCost="Rs.",str( PayTax + Totalcost + Ser_Charge)
+    PaidTax="Rs.",str('%.2f'% PayTax)
+
+    Service_Charge.set(Service)
+    cost.set(costofmeal)
+    Tax.set(PaidTax)
+    Subtotal.set(costofmeal)
+    Total.set(OverAllCost)
+
